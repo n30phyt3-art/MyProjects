7bcbdb748bcb992401417cd207881bf7b4816548
n30phyt3-art <62809552+n30phyt3-art@users.noreply.github.com>
2020-03-28 22:11:25 +0400

    Create Program.cs

---
+++ IntVariable/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int my_fucking_variable = 10;
+            Console.WriteLine(my_fucking_variable);
+            Console.ReadKey();
+        }
+    }
+}
